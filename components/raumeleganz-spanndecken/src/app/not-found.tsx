192ddb88a1ee00d834d3c4d1c2be63c9d271e319
Dani Temera <dani.temera@icloud.com>
2026-04-30 05:00:12 +0200

    fix searchParams crash

--- components/raumeleganz-spanndecken/src/app/not-found.tsx
+++ components/raumeleganz-spanndecken/src/app/not-found.tsx
@@ -1,13 +1,3 @@
-"use client";
-
-import Link from "next/link";
-
 export default function NotFound() {
-    return (
-        <div>
-            <h1>404 - Seite nicht gefunden</h1>
-            <p>Die angeforderte Seite konnte nicht gefunden werden.</p>
-            <Link href="/">Zur Startseite</Link>
-        </div>
-    );
-}
+    return <div>404</div>;
+  }
