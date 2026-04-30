1fcbbb2089a033630a181d57c58a80e56be29eeb
Dani Temera <dani.temera@icloud.com>
2026-04-30 05:25:59 +0200

    remove broken not-found page

--- components/raumeleganz-spanndecken/src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-    return (
-      <div style={{ padding: "40px", textAlign: "center" }}>
-        <h1>404 - Seite nicht gefunden</h1>
-        <p>Die Seite existiert nicht oder wurde verschoben.</p>
-        <a href="/">Zur Startseite</a>
-      </div>
-    );
-  }
