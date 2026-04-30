30c13a8f85fa83b863e57b202eec97d9c7456f96
Dani Temera <dani.temera@icloud.com>
2026-04-30 04:54:41 +0200

    remove eslint config

--- components/raumeleganz-spanndecken/src/app/not-found.tsx
+++ components/raumeleganz-spanndecken/src/app/not-found.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import Link from "next/link";
+
 export default function NotFound() {
-    return <div>Seite nicht gefunden</div>;
-  }
+    return (
+        <div>
+            <h1>404 - Seite nicht gefunden</h1>
+            <p>Die angeforderte Seite konnte nicht gefunden werden.</p>
+            <Link href="/">Zur Startseite</Link>
+        </div>
+    );
+}
